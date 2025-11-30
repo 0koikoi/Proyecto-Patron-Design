21137a6f498de6e6e8e39086fd68b6ff27202a03
Apofis-coder <salazarleon.luisangel2005@gmail.com>
2025-11-30 00:38:43 -0500

    dentro de la carpeta strategy se creo la interface RespuestaStrategy y su metodo responder, y las diversas clase que la implementan como SaludoStrategy, VentasStrategy, SoporteStrategy y DefaultStraegy

---
+++ src/main/java/com/callcenter/callcenter/ivr/strategy/SoporteStrategy.java
@@ -0,0 +1,9 @@
+package com.callcenter.callcenter.ivr.strategy;
+
+public class SoporteStrategy implements RespuestaStrategy{
+
+    @Override
+    public String responder(String opcion){
+        return "";
+    }
+}
---
+++ src/main/java/com/callcenter/callcenter/ivr/strategy/VentasStrategy.java
@@ -0,0 +1,9 @@
+package com.callcenter.callcenter.ivr.strategy;
+
+public class VentasStrategy implements RespuestaStrategy {
+
+    @Override
+    public String responder(String opcion) {
+        return "";
+    }
+}
